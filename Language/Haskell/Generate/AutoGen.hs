34b54aa84f42caac400eb46b21e64c5f69400960
Beerend Lauwers <beerend.lauwers@capgemini.com>
2015-04-01 16:50:28 +0200

    - Switch from Symbol to Ident to get rid of the () in output - Allow for pragmas and non-qualified imports

--- Language/Haskell/Generate/AutoGen.hs
+++ Language/Haskell/Generate/AutoGen.hs
@@ -1,8 +1,9 @@
 module Language.Haskell.Generate.AutoGen (saveModuleToFile) where
 
 import Language.Haskell.Generate.AutoGen.Extra
+import Language.Haskell.Exts.Syntax (ModulePragma)
 import System.IO (writeFile)
 import Language.Haskell.Generate.Monad (ModuleG, generateModule)
 
-saveModuleToFile :: (ModuleG,String) -> FilePath -> IO ()
-saveModuleToFile (m,nm) path = writeFile path (generateModule m nm)
+saveModuleToFile :: [String] -> [ModulePragma] -> (ModuleG,String) -> FilePath -> IO ()
+saveModuleToFile noquals pragma (m,nm) path = writeFile path (generateModule' noquals pragma m nm)
--- Language/Haskell/Generate/AutoGen/Extra.hs
+++ Language/Haskell/Generate/AutoGen/Extra.hs
@@ -1,13 +1,44 @@
 module Language.Haskell.Generate.AutoGen.Extra where
 
+import Control.Monad.Trans.Writer
 import Language.Haskell.Exts.Syntax
 import Language.Haskell.Exts.SrcLoc
+import Language.Haskell.Exts.Pretty
+import Language.Haskell.Generate.Monad
+import qualified Data.Set as S
 
 mkIntLit i = Lit (Int i)
 
-mkQName s = UnQual (Symbol s)
+mkQName s = UnQual (Ident s)
 
 mkClassInstance :: String -> [String] -> [InstDecl] -> Decl
 mkClassInstance cls names decls = InstDecl noLoc Nothing [] [] (mkQName cls) (map (TyCon . mkQName) names) decls
 
-mkMatch name exp = Match noLoc (Symbol name) [] Nothing (UnGuardedRhs exp) (BDecls [])
+mkDataDecl nm pats constrs = DataDecl noLoc DataType [] (Ident nm) pats rhs' []
+ where rhs' = map (\constr -> QualConDecl noLoc [] [] (ConDecl (Ident constr) [])) constrs
+
+mkMatch nm pats exp = Match noLoc (Ident nm) pats Nothing (UnGuardedRhs exp) (BDecls [])
+
+mkPragma :: [ValidPragma] -> ModulePragma
+mkPragma ps = LanguagePragma noLoc (map (Ident . show) ps)
+
+data ValidPragma = MultiParamTypeClasses | FunctionalDependencies
+ deriving Show
+
+-- TODO: Add as patch to haskell-generate
+addDecl :: Decl -> ModuleM ()
+addDecl inst = ModuleM $ do
+ tell (S.empty, [inst])
+ 
+addImport imp = ModuleM $ do
+ tell (S.singleton (ModuleName imp), [])
+ 
+generateModule' :: [String] -> [ModulePragma] -> ModuleG -> String -> String
+generateModule' noquals pragmas = fmap prettyPrint . (runModuleM' noquals pragmas)
+ 
+runModuleM' :: [String] -> [ModulePragma] -> ModuleG -> String -> Module
+runModuleM' noquals pragmas (ModuleM act) name = 
+  Module noLoc (ModuleName name) pragmas Nothing export (map (\(qual,md) -> ImportDecl noLoc md qual False False Nothing Nothing Nothing) imps'') decls
+  where (export, (imps, decls)) = runWriter act
+        imps' = map (\(ModuleName i) -> notElem i noquals) (S.toList imps)
+        imps'' = zip imps' (S.toList imps)
